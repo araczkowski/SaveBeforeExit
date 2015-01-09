45cc71ce98196cd56e38be9292fba9d66aa99341
Andrzej Raczkowski <araczkowski@gmail.com>
2015-01-09 17:09:10 +0100

    special treatment for old IE - TODO (onchange problem)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,27 @@
 'use strict';
 //var apex, CKEDITOR, $v;
+
+function detectIE() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    if (trident > 0) {
+        // IE 11 (or newer) => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
+
 (function ($) {
     $.widget('ui.apexSaveBeforeExit', {
         options: {
@@ -142,6 +164,9 @@
                 });
 
             // Revert Modifications - on change
+            if (detectIE() === 7) {
+                // special treatment for old IE - TODO
+            }
             $(uiw.options.revertModificationsSelector)
                 .each(function () {
                     $(this).change(function (event) {
